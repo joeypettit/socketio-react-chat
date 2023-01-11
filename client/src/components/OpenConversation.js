e7c31115edcf33d942374f4621ee53816c368fd6
Joey Pettit <pettitbass@gmail.com>
2023-01-11 15:52:07 -0600

    messaging scrolling now scrolls to most recent message automatically

--- client/src/components/OpenConversation.js
+++ client/src/components/OpenConversation.js
@@ -1,63 +1,85 @@
-import React, {useState} from 'react'
-import { Form, InputGroup, Button } from 'react-bootstrap';
-import { useConversations } from './contexts/ConversationsProvider';
+import React, { useState, useCallback } from "react";
+import { Form, InputGroup, Button } from "react-bootstrap";
+import { useConversations } from "./contexts/ConversationsProvider";
 
 function OpenConversation() {
-    const [text, setText] = useState('');
-    const {sendMessage, selectedConversation} = useConversations();
+  // state to manage text input
+  const [text, setText] = useState("");
+  // pull sendMessage function and selectedConversation from conversations provider
+  const { sendMessage, selectedConversation } = useConversations();
 
-    function handleSubmit(e){
-        e.preventDefault();
-
-        sendMessage(selectedConversation.recipients.map((recipient)=>{
-            return recipient.id
-        }), text)
-
-        setText('');
+  // this function takes in the final div message DOM node as its argument
+  // and scrolls to this node on rerender
+  function scroll(node) {
+    if (node) {
+      node.scrollIntoView({ behavior: "smooth" });
     }
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    sendMessage(
+      selectedConversation.recipients.map((recipient) => {
+        return recipient.id;
+      }),
+      text
+    );
+
+    setText("");
+  }
 
   return (
     <div className="d-flex flex-column flex-grow-1">
-        <div className="flex-grow-1 overflow-auto">
-            <div className='d-flex flex-column align-items-start justify-content-end px-3'>
-                {selectedConversation.messages.map((message, index)=>{
-                    return(
-                        <div
-                            key={index}
-                            className={`my-1 d-flex flex-column ${message.fromMe ? 'align-self-end':''}`}
-                        >
-                            <div 
-                                className={`rounded px-2 py-1 ${message.fromMe ? `bg-primary text-white` : `border`}`}>
-                                {message.text}
-                            </div>
-                            <div className={`text-muted small ${message.fromMe ? 'text-right': ''}`}>
-                                {message.fromMe ? 'You' : message.senderName}
-                            </div>
-                        </div>
-                    
-
-                    )
-                })}
-
-            </div>
-
+      <div className="flex-grow-1 overflow-auto">
+        <div className="d-flex flex-column align-items-start justify-content-end px-3">
+          {selectedConversation.messages.map((message, index) => {
+            // will be true for last message
+            const lastMessage =
+              selectedConversation.messages.length - 1 === index;
+            return (
+              <div
+                key={index}
+                ref={lastMessage ? scroll : null}
+                className={`my-1 d-flex flex-column ${
+                  message.fromMe ? "align-self-end" : ""
+                }`}
+              >
+                <div
+                  className={`rounded px-2 py-1 ${
+                    message.fromMe ? `bg-primary text-white` : `border`
+                  }`}
+                >
+                  {message.text}
+                </div>
+                <div
+                  className={`text-muted small ${
+                    message.fromMe ? "text-right" : ""
+                  }`}
+                >
+                  {message.fromMe ? "You" : message.senderName}
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className='m-2'>
-                <InputGroup>
-                <Form.Control 
-                    as="textarea" 
-                    required 
-                    value={text} 
-                    onChange={e=>setText(e.target.value)}
-                    style={{height: '75px', resize: 'none'}}
-                />
-                    <Button type="submit">Send</Button>
-                </InputGroup>
-            </Form.Group>
-        </Form>
-     </div>
-  )
+      </div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="m-2">
+          <InputGroup>
+            <Form.Control
+              as="textarea"
+              required
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              style={{ height: "75px", resize: "none" }}
+            />
+            <Button type="submit">Send</Button>
+          </InputGroup>
+        </Form.Group>
+      </Form>
+    </div>
+  );
 }
 
 export default OpenConversation;
